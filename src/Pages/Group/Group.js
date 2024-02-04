71f98df066f25823bf9af5667f1e4d90ac139bb1
ahmedullah12 <gulamjoy321@gmail.com>
2024-02-05 01:26:02 +0600

    header added

---
+++ src/Pages/Group/Group.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../component/Header/Header';
+import GroupBanner from '../../component/GroupBanner/GroupBanner';
+
+const Group = () => {
+    return (
+        <div>
+            <Header></Header>
+            <GroupBanner></GroupBanner>
+        </div>
+    );
+};
+
+export default Group;
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,15 +1,34 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../assets/logo.png'
+import { Form, FormControl, InputGroup } from 'react-bootstrap';
+import { IoMdSearch } from "react-icons/io";
 
 function Header() {
   return (
-    <Navbar className="bg-body-tertiary">
+    <Navbar className="bg-white ">
       <Container>
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="" />
+        </Navbar.Brand>
         <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
+        <Navbar.Collapse className="justify-content-center mt-2">
+          <Form className="d-flex" style={{ width: '360px' }}>
+            <InputGroup style={{position: "relative"}}>
+            
+                <FormControl
+                  placeholder="Search for your favorite groups in ATG"
+                  aria-label="Search"
+                  aria-describedby="basic-addon1"
+                  style={{width: "360px", height: "42px",borderRadius: "21px", paddingLeft: "40px", backgroundColor: "#F2F2F2"}}
+                />
+                <IoMdSearch style={{position: "absolute", top: "10px", left: "10px"}} size={25}></IoMdSearch>
+            </InputGroup>
+          </Form>
+        </Navbar.Collapse>
+        <Navbar.Collapse className="justify-content-end" style={{ maxWidth: '180px' }}>
+          <Navbar.Text className='text-black'>
+            Create Account. Its Free
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
